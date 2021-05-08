c755ace0994bd0015d8c56f43fe0322148e59a82
Cian Payne <cianjpayne@gmail.com>
2021-05-08 14:24:01 +0100

    Refactored BST.hs so everything is ordered nice and neatly (alphabetically by exported and non-exported)

--- src/BST.hs
+++ src/BST.hs
@@ -17,9 +17,13 @@ import Prelude hiding (lookup)
 -- Defining the data type
 data BST k v = Leaf | Node k v (BST k v) (BST k v) deriving Show 
 
-isEmpty :: BST k v -> Bool 
-isEmpty Leaf = True
-isEmpty _    = False
+---------------------------------------------------------------------------------
+-- Exported functions 
+---------------------------------------------------------------------------------
+
+elements :: (Ord k) => BST k v -> [(k, v)] 
+elements Leaf             = []
+elements (Node k v t1 t2) = elements t1 ++ [(k, v)] ++ elements t2
 
 empty :: BST k v
 empty = Leaf
@@ -31,6 +35,10 @@ insert key value (Node k v t1 t2)
                         | key < k    = Node k v (insert key value t1) t2
                         | otherwise  = Node key value t1 t2
 
+isEmpty :: BST k v -> Bool 
+isEmpty Leaf = True
+isEmpty _    = False
+
 
 lookup :: (Ord k) =>  k -> BST k v -> Maybe v
 lookup _   Leaf                      = Nothing
@@ -39,16 +47,28 @@ lookup key (Node k v t1 t2)
                         | key < k    = lookup key t1
                         | otherwise  = Just v
 
-elements :: (Ord k) => BST k v -> [(k, v)] 
-elements Leaf             = []
-elements (Node k v t1 t2) = elements t1 ++ [(k, v)] ++ elements t2
-
 remove :: (Ord k) => k -> BST k v -> BST k v
 remove _   Leaf                       = Leaf
 remove key (Node k v t1 t2) 
                         | key < k = Node k v (remove key t1) t2 
                         | key > k = Node k v t1              (remove key t2) 
                         | otherwise = removeNode (Node k v t1 t2)
+
+removeIf :: (Ord k) => (k -> Bool) -> BST k v -> BST k v
+removeIf _  Leaf                    = Leaf
+removeIf p (Node k v t1 t2)
+                        | p k       =
+                            removeNode (Node k v (removeIf p t1) (removeIf p t2))
+                        | otherwise =
+                            Node k v (removeIf p t1) (removeIf p t2)
+
+---------------------------------------------------------------------------------
+-- Helper functions, not exported 
+---------------------------------------------------------------------------------
+
+minElement :: (Ord k) => BST k v -> (k, v) 
+minElement (Node k v Leaf _) = (k, v) 
+minElement (Node k v t1   _) = minElement t1 
 
 removeNode :: (Ord k) => BST k v -> BST k v
 removeNode Leaf = Leaf
@@ -61,14 +81,3 @@ removeNode (Node _ _ t1     t2) = let element = minElement t2
                                       t2'     = remove key t2
                                       in Node key value t1 t2'
 
-minElement :: (Ord k) => BST k v -> (k, v) 
-minElement (Node k v Leaf _) = (k, v) 
-minElement (Node k v t1   _) = minElement t1 
-
-removeIf :: (Ord k) => (k -> Bool) -> BST k v -> BST k v
-removeIf _  Leaf                    = Leaf
-removeIf p (Node k v t1 t2)
-                        | p k       =
-                            removeNode (Node k v (removeIf p t1) (removeIf p t2))
-                        | otherwise =
-                            Node k v (removeIf p t1) (removeIf p t2)
